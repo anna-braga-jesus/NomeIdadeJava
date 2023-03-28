18f27580a3539c9429c7a71042bd7da3c35b6a67
anna-braga-jesus <annajesus.nave@gmail.com>
2023-03-28 10:25:58 -0300

    feat: add exercise

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    Scanner scan = new Scanner(System.in);
+    String nome;
+    int idade;
+
+        while(true){
+            System.out.println("Nome: ");
+            nome = scan.next();
+            if(nome.equals("0")) break;
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+        System.out.println("Continua aqui...");
+
+    }
+    
+}
+
